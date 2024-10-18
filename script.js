43754c10f9730402cfc9b39a6b51b0568d744898
Altamash Ahmad <161801681+AhmadAltamash@users.noreply.github.com>
2024-10-18 23:47:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function toggles(){
     let open = document.querySelector(".open")
     let close = document.querySelector(".close")
-    let links = document.querySelector(".links")
-    links.style.display = "none"
-    close.style.display = "none"
+    let links = document.querySelector(".nav")
+    //links.style.display = "none"
+    open.style.display = "none"
     open.addEventListener("click",()=>{
         open.style.display = "none"
         close.style.display = "block"
-        links.style.display = "flex"
+        links.style.display = "block"
     })
     close.addEventListener("click",()=>{
         open.style.display = "block"
@@ -16,3 +16,22 @@ function toggles(){
     })
 }
 toggles()
+
+function navigate(){
+    let nav = document.querySelector('.nav');
+    let items = document.querySelectorAll("li");
+    items.forEach(item =>{
+        item.addEventListener("click",()=>{
+            let oldActiveItem = nav.querySelector('li.active')
+            if(oldActiveItem) oldActiveItem.classList.remove('active');
+            item.classList.add('active');
+            nav.style.setProperty('--position-x-active', item.offsetLeft + 'px')
+            let lists = document.querySelectorAll('.item');
+            document.getElementById('slide').appendChild(lists[0]);
+        })
+    })
+    let itemActive = nav.querySelector('li.active')
+    if(itemActive) nav.style.setProperty('--position-x-active', itemActive.offsetLeft + 'px')
+}
+navigate()
+
